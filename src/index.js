895cebe24b0329a8104f5a070e28c6f26975f657
KhoaTT <khoanono963@gmail.com>
2022-09-26 15:10:45 +0700

    ok2

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-const path = require('path');
-const express = require('express');
-const handlebars = require('express-handlebars');
-const morgan = require('morgan');
-const route = require('./routes');
-const app = express();
-const port = 3000;
+const path = require('path')
+const express = require('express')
+const handlebars = require('express-handlebars')
+const morgan = require('morgan')
+const route = require('./routes')
+const app = express()
+const port = 3000
 
-app.use(morgan('combined'));
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-);
-app.use(express.json());
-app.engine(
-    'hbs',
-    handlebars.engine({
-        extname: '.hbs',
-    }),
-);
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.use(morgan('combined'))
+app.use(express.urlencoded({
+  extended: true
+}))
+  app.use(express.json())
+app.engine('hbs',handlebars.engine({
+  extname: '.hbs'
+})); 
+app.set('view engine','hbs')
+app.set('views',path.join(__dirname,'resources\\views'))
 // console.log(path.join(__dirname,'views'))
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')))
+
 
 //ROUTE
-route(app);
+route(app)
 
-        app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+// })tening on port ${port}`)
+// })tening on port ${port}`)
+})
